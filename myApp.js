7780b7f9199741aaebeac015771c32999a18fd2c
larnee <larnee@mailfence.com>
2022-06-19 14:05:19 +0200

    restart

--- myApp.js
+++ myApp.js
@@ -1,39 +1,44 @@
 let express = require('express');
+require('dotenv').config();
 let app = express();
 
+// Solution 7
+app.get('/', function(req, res, next) {
+	console.log(req.method + " " + req.path + " - " + req.ip);
+	next();
+});
+
+// Solution 6
+app.get('/json', function(req, res) {
+	if ( process.env.MESSAGE_STYLE === "uppercase" ) {
+		res.json({ message: "HELLO JSON" });
+	} else {
+		res.json({ message: "Hello json" });
+	}
+});
+
+/* Solution 5
+app.get('/json', function(req, res) {
+	res.json({ message: "Hello json" });
+}); */
+
+// Solution 4
+publicPath = __dirname + "/public";
+app.use('/public', express.static(publicPath));
 
 
+// Solution 3
+indexPath = __dirname + "/views/index.html";
+app.get('/', function(req, res) { res.sendFile(indexPath); });
 
+/* Solution 2
+app.get("/", function(req, res) {
+  res.send('Hello Express');
+});
 
+/* Solution 1
+console.log("Hello World");				
+*/
 
+module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
